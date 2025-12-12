367af0ae3cd3adc9d93b7a6f5fe34d659c623a85
Leftmy <leftmyfrid0708@gmail.com>
2025-12-12 21:21:29 +0200

    Stable 1.0

---
+++ Models/UserList.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MakeAList.Models
+{
+    public class UserList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = "";
+
+        public ListType Type { get; set; }
+
+        public string UserId { get; set; } = "";
+        public IdentityUser? User { get; set; }
+
+        public List<ListItem> Items { get; set; } = new();
+    }
+}
